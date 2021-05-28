4a92f9f649dc55cbfa194d5b30946dba89ba328b
Jhoan Caramantin <jhoan1915@gmail.com>
2021-05-28 08:51:59 -0500

    Initial commit

---
+++ SocialMedia.Infrastructure/Repositories/PostMongoRepository.cs
@@ -0,0 +1,31 @@
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;  
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    class PostMongoRepository
+    {
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description Mongo{x}",
+                Date = DateTime.Now,
+                Image = "",
+                UserId = x * 2
+
+            });
+
+            await Task.Delay(10);
+
+            return posts;
+        }
+
+    }
+}
---
+++ SocialMediaApi/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Repositories;
+
+namespace SocialMediaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+
+        public readonly IPostRepository _postRepository;
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public IActionResult GetPosts()
+        {
+            var posts = _postRepository.GetPosts();
+            return Ok(posts);
+        }
+
+
+    }
+}
