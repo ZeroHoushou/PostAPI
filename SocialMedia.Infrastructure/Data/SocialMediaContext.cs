b7c1519d3f46fe28222a6e4d8d962673be262f53
Jhoan Caramantin <jhoan1915@gmail.com>
2021-06-02 12:04:52 -0500

    Fluent API en EF Core

--- SocialMedia.Infrastructure/Data/SocialMediaContext.cs
+++ SocialMedia.Infrastructure/Data/SocialMediaContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using SocialMedia.Core.Entities;
+using SocialMedia.Infrastructure.Data.Configurations;
 
 namespace SocialMedia.Infrastructure.Data
 {
@@ -16,9 +17,9 @@ namespace SocialMedia.Infrastructure.Data
         {
         }
 
-        public virtual DbSet<Comentario> Comentarios { get; set; }
-        public virtual DbSet<Publicacion> Publicacion { get; set; }
-        public virtual DbSet<Usuario> Usuarios { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<User> Users { get; set; }
 
         
 
@@ -26,87 +27,13 @@ namespace SocialMedia.Infrastructure.Data
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
 
-            modelBuilder.Entity<Comentario>(entity =>
-            {
-                entity.HasKey(e => e.IdComentario);
+            modelBuilder.ApplyConfiguration(new CommentConfiguration());
 
-                entity.ToTable("Comentario");
+            modelBuilder.ApplyConfiguration(new PostConfiguration());
 
-                entity.Property(e => e.IdComentario).ValueGeneratedNever();
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
 
-                entity.Property(e => e.Descripcion)
-                    .IsRequired()
-                    .HasMaxLength(500)
-                    .IsUnicode(false);
 
-                entity.Property(e => e.Fecha).HasColumnType("datetime");
-
-                entity.HasOne(d => d.IdPublicacionNavigation)
-                    .WithMany(p => p.Comentarios)
-                    .HasForeignKey(d => d.IdPublicacion)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Comentario_Publicacion");
-
-                entity.HasOne(d => d.IdUsuarioNavigation)
-                    .WithMany(p => p.Comentarios)
-                    .HasForeignKey(d => d.IdUsuario)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Comentario_Usuario");
-            });
-
-            modelBuilder.Entity<Publicacion>(entity =>
-            {
-                entity.HasKey(e => e.IdPublicacion);
-
-                entity.ToTable("Publicacion");
-
-                entity.Property(e => e.Descripcion)
-                    .IsRequired()
-                    .HasMaxLength(1000)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Fecha).HasColumnType("datetime");
-
-                entity.Property(e => e.Imagen)
-                    .HasMaxLength(500)
-                    .IsUnicode(false);
-
-                entity.HasOne(d => d.IdUsuarioNavigation)
-                    .WithMany(p => p.Publicacions)
-                    .HasForeignKey(d => d.IdUsuario)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Publicacion_Usuario");
-            });
-
-            modelBuilder.Entity<Usuario>(entity =>
-            {
-                entity.HasKey(e => e.IdUsuario);
-
-                entity.ToTable("Usuario");
-
-                entity.Property(e => e.Apellidos)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Email)
-                    .IsRequired()
-                    .HasMaxLength(30)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.FechaNacimiento).HasColumnType("date");
-
-                entity.Property(e => e.Nombres)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Telefono)
-                    .HasMaxLength(10)
-                    .IsUnicode(false);
-            });
-
-           
         }
 
        
