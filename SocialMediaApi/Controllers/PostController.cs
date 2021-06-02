18cf9c7153c491a1c21c06fb699182754a0a7a5c
Jhoan Caramantin <jhoan1915@gmail.com>
2021-06-02 16:43:55 -0500

    Implementation DTOs in PostController

--- SocialMediaApi/Controllers/PostController.cs
+++ SocialMediaApi/Controllers/PostController.cs
@@ -2,6 +2,8 @@
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using System.Threading.Tasks;
+using SocialMedia.Core.DTOs;
+using System.Linq;
 
 namespace SocialMediaApi.Controllers
 {
@@ -20,19 +22,46 @@ namespace SocialMediaApi.Controllers
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            return Ok(posts);
+            var postsDto = posts.Select(x => new PostDto
+            {
+                PostId=x.PostId,
+                Date=x.Date,
+                Description=x.Description,
+                Image=x.Image,
+                UserId=x.UserId
+
+            });
+            return Ok(postsDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
-            var posts = await _postRepository.GetPost(id);
-            return Ok(posts);
+            var post = await _postRepository.GetPost(id);
+            var postsDto = new PostDto
+            {
+                PostId = post.PostId,
+                Date = post.Date,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId
+
+            };
+            return Ok(postsDto);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            var post = new Post
+            {
+                PostId = postDto.PostId,
+                Date = postDto.Date,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId
+
+            };
             await _postRepository.InserPost(post);
             return Ok(post);
         }
