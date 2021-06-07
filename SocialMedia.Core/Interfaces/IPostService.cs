f18c3269a88f3b7138ba597c8c117d4d6d8e1005
Jhoan Caramantin <jhoan1915@gmail.com>
2021-06-07 16:00:47 -0500

    Interface IPostService add

---
+++ SocialMedia.Core/Interfaces/IPostService.cs
@@ -0,0 +1,10 @@
+﻿using SocialMedia.Core.Entities;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.Interfaces
+{
+    public interface IPostService
+    {
+        Task InsertPost(Post post);
+    }
+}
---
+++ SocialMedia.Core/Services/PostService.cs
@@ -0,0 +1,26 @@
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly IPostRepository _postRepository;
+        public PostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+
+        }
+
+        public async Task InsertPost(Post post)
+        {
+
+            await _postRepository.InsertPost(post);
+        }
+        
+    }
+}
